9c64f86a7af76d6a5f4cb8a394d5594b134c6827
coolguy101 <jiang_501@hotmail.com>
2014-01-08 12:51:03 -0700

    commited my assginment 1

---
+++ Wocao/src/com/example1/wocao/CustomAdapter.java
@@ -0,0 +1,45 @@
+package com.example1.wocao;
+
+
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+ 
+public class CustomAdapter extends ArrayAdapter<counter>{
+	public LayoutInflater inflater = null;
+	public ArrayList<counter> somearray;
+	public TextView thetextview=null;
+	public CustomAdapter(Context context, int resource,ArrayList<counter> thearray) 
+	{
+		super(context, resource,thearray);
+		this.somearray=thearray;
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		
+		View theview = convertView; 
+		if(theview==null)
+			{
+			  theview = inflater.inflate(R.layout.item2, null);
+			}
+		counter obj_counter = somearray.get(position);
+		String thename= obj_counter.name1;
+		((TextView) theview.findViewById(R.id.wocao)).setText(thename);
+		convertView = theview; 
+		
+		return convertView;
+	}
+	
+    
+}
